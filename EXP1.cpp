4f382f23a50a2264219c575ce6a9fad32f3a7a1c
Sarvesh03008 <140037878+Sarvesh03008@users.noreply.github.com>
2023-07-20 16:15:46 +0530

    Create EXP1.cpp

---
+++ EXP1.cpp
@@ -0,0 +1,55 @@
+22070123103
+Sarvesh Dhote
+EXP 1 
+
+
+//CODE 1//
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    cout<<"My first program";
+    return 0;
+}
+
+//OUTPUT 1//
+Hello World!
+My first program
+PS D:\22070123103> 
+
+//CODE 2//
+#include<iostream>
+using namespace std;
+int a;
+int b;
+int sum;
+int main()
+{
+    cout<<"Enter First Number";
+    cin>>a;
+    cout<<"Enter Second Number";
+    cin>>b;
+    sum = a + b ;
+    cout<<"Sum Of A and B is"<<sum;
+}
+
+//OUTPUT 2//
+Enter First Number 10
+Enter Second Number 15
+Sum Of A and B is 25
+PS D:\22070123103>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
